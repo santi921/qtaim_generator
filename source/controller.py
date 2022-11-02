2832b4c0df980849313496afca49669fe9fcfdca
Santiago Vargas <santiagovargas@pop-os.localdomain>
2022-11-01 17:01:54 -0700

    update on controller + change permissions on exe

--- source/controller.py
+++ source/controller.py
@@ -1,53 +1,82 @@
-import os, random, threading, subprocess 
+import os, random, threading, subprocess
 from glob import glob
 
-folders = [ name for name in os.listdir("./") if os.path.isdir(os.path.join('./', name)) ]
+folders = [name for name in os.listdir("./") if os.path.isdir(os.path.join("./", name))]
+
 
 class ThreadWithResult(threading.Thread):
-    def __init__(self, group=None, target=None, name=None, args=(), kwargs={}, *, daemon=None):
+    def __init__(
+        self, group=None, target=None, name=None, args=(), kwargs={}, *, daemon=None
+    ):
         def function():
             self.result = target(*args, **kwargs)
+
         super().__init__(group=group, target=function, name=name, daemon=daemon)
 
 
 def controller_single(dir_active):
 
-    #get random folder from dir_active
+    # get random folder from dir_active
     for i in range(1000):
-        folders = [ name for name in os.listdir(dir_active) if os.path.isdir(os.path.join(dir_active, name)) ]
+        folders = [
+            name
+            for name in os.listdir(dir_active)
+            if os.path.isdir(os.path.join(dir_active, name))
+        ]
 
-        #while(cond):
+        # while(cond):
         folder_choice = random.choice(folders)
-        #    print(os.path.exists(folder_choice + "/input.wfn"))
-        #    cond =  os.path.exists(folder_choice + "/input.wfn")
+        print(folder_choice)
+        os.system(
+            "orca "
+            + dir_active
+            + folder_choice
+            + "/reactants"
+            + "/input.in > "
+            + dir_active
+            + folder_choice
+            + "/reactants/output.out"
+        )
+        os.system(
+            "orca "
+            + dir_active
+            + folder_choice
+            + "/products"
+            + "/input.in > "
+            + dir_active
+            + folder_choice
+            + "/products/output.out"
+        )
+        # cond_qtaim = os.path.exists(folder_choice + "/out")
 
-        
-        print(folder_choice)    
-        os.system("orca " + dir_active + folder_choice + "/reactants" +  "/input.in" )
-        os.system("orca " + dir_active + folder_choice + "/products" +  "/input.in" )
-        #cond_qtaim = os.path.exists(folder_choice + "/out")
-
-        subprocess.run(dir_active + folder_choice + "/reactants" + '/props.sh')
+        subprocess.run(dir_active + folder_choice + "/reactants" + "/props.sh")
         os.system("mv " + "./CPprop.txt " + dir_active + folder_choice + "/reactants/")
 
-        subprocess.run(dir_active + folder_choice + "/products" + '/props.sh')
+        subprocess.run(dir_active + folder_choice + "/products" + "/props.sh")
         os.system("mv " + "./CPprop.txt " + dir_active + folder_choice + "/products/")
-        # move CPprop.txt to folder 
+        # move CPprop.txt to folder
 
 
 def main():
 
     dir_active = "../data/hydro/QTAIM/"
     for i in range(1000):
-        counter = 0 
-        t1 = ThreadWithResult(target=controller_single, kwargs={"dir_active" : dir_active})
-        t2 = ThreadWithResult(target=controller_single, kwargs={"dir_active" : dir_active})
-        t3 = ThreadWithResult(target=controller_single, kwargs={"dir_active" : dir_active})
+        counter = 0
+        t1 = ThreadWithResult(
+            target=controller_single, kwargs={"dir_active": dir_active}
+        )
+        t2 = ThreadWithResult(
+            target=controller_single, kwargs={"dir_active": dir_active}
+        )
+        t3 = ThreadWithResult(
+            target=controller_single, kwargs={"dir_active": dir_active}
+        )
         t1.start()
         t2.start()
         t3.start()
         t1.join()
         t2.join()
         t3.join()
+
 
 main()
--- source/create_files.py
+++ source/create_files.py
@@ -1,29 +1,31 @@
-import pandas as pd 
-import numpy as np 
-from glob import glob 
+import pandas as pd
+import numpy as np
+from glob import glob
 import subprocess
-import os 
+import os
+import stat
 import random
+
 
 def main():
     json_tf = True
-    if(json_tf): 
+    if json_tf:
         json_loc = "../data/hydro/"
         json_file = json_loc + "rev_corrected_bonds_qm_9_hydro_training.json"
         pandas_file = pd.read_json(json_file)
 
         for ind, _ in pandas_file.iterrows():
-            
+
             ind_random = random.choice(range(len(pandas_file)))
-            
+
             row = pandas_file.iloc[ind_random]
             reaction_id = row["reaction_id"]
-            # create folder in json directory for each reaction 
+            # create folder in json directory for each reaction
             QTAIM_loc = json_loc + "QTAIM/"
             QTAIM_loc_reactant = json_loc + "QTAIM/" + str(reaction_id) + "/reactants/"
             QTAIM_loc_product = json_loc + "QTAIM/" + str(reaction_id) + "/products/"
             # create folder for each reaction + reactants + products
-            
+
             if not os.path.exists(QTAIM_loc):
                 os.mkdir(QTAIM_loc)
             if not os.path.exists(json_loc + "QTAIM/" + str(reaction_id)):
@@ -33,57 +35,91 @@ def main():
             if not os.path.exists(QTAIM_loc_product):
                 os.mkdir(QTAIM_loc_product)
 
-
             # reactants
             reactants = row["combined_reactants_graph"]
             atoms = int(len(reactants["molecule"]["sites"]))
-            with open(QTAIM_loc_reactant + "/input.in", 'w') as f:
+            with open(QTAIM_loc_reactant + "/input.in", "w") as f:
                 f.write("!B3LYP def2-SVP AIM\n")
                 f.write("%SCF\n")
                 f.write("    MaxIter 1000\n")
                 f.write("END\n")
-                f.write("* xyz {} {}\n".format(reactants["molecule"]["charge"], reactants["molecule"]["spin_multiplicity"]))
-                for ind in range(atoms): 
+                f.write(
+                    "* xyz {} {}\n".format(
+                        reactants["molecule"]["charge"],
+                        reactants["molecule"]["spin_multiplicity"],
+                    )
+                )
+                for ind in range(atoms):
                     xyz = reactants["molecule"]["sites"][ind]["xyz"]
                     atom = reactants["molecule"]["sites"][ind]["species"][0]["element"]
-                    f.write("{}\t{: .4f}\t{: .4f}\t{: .4f}\n".format(atom, xyz[0], xyz[1], xyz[2]))
-                f.write("*\n") 
-
+                    f.write(
+                        "{}\t{: .4f}\t{: .4f}\t{: .4f}\n".format(
+                            atom, xyz[0], xyz[1], xyz[2]
+                        )
+                    )
+                f.write("*\n")
 
             # products
             products = row["combined_products_graph"]
             atoms = int(len(products["molecule"]["sites"]))
-            with open(QTAIM_loc_product + "/input.in", 'w') as f:
+            with open(QTAIM_loc_product + "/input.in", "w") as f:
                 f.write("!B3LYP def2-SVP AIM\n")
                 f.write("%SCF\n")
                 f.write("    MaxIter 1000\n")
                 f.write("END\n")
-                f.write("* xyz {} {}\n".format(products["molecule"]["charge"], products["molecule"]["spin_multiplicity"]))
-                for ind in range(atoms): 
+                f.write(
+                    "* xyz {} {}\n".format(
+                        products["molecule"]["charge"],
+                        products["molecule"]["spin_multiplicity"],
+                    )
+                )
+                for ind in range(atoms):
                     xyz = products["molecule"]["sites"][ind]["xyz"]
                     atom = products["molecule"]["sites"][ind]["species"][0]["element"]
-                    f.write("{}\t{: .4f}\t{: .4f}\t{: .4f}\n".format(atom, xyz[0], xyz[1], xyz[2]))
-                f.write("*\n") 
-
+                    f.write(
+                        "{}\t{: .4f}\t{: .4f}\t{: .4f}\n".format(
+                            atom, xyz[0], xyz[1], xyz[2]
+                        )
+                    )
+                f.write("*\n")
 
             # run QTAIM on reactants
-            with open(QTAIM_loc_reactant + '/props.sh', 'w') as f: 
+            with open(QTAIM_loc_reactant + "/props.sh", "w") as f:
                 f.write("#!/bin/bash\n")
-                f.write("./Multiwfn_3.8_dev_bin_Linux_noGUI/Multiwfn " + QTAIM_loc_reactant + "/input.wfn < ./Multiwfn_3.8_dev_bin_Linux_noGUI/data.txt | tee ./" + QTAIM_loc_reactant + "out \n")
+                f.write(
+                    "./Multiwfn_3.8_dev_bin_Linux_noGUI/Multiwfn "
+                    + QTAIM_loc_reactant
+                    + "/input.wfn < ./Multiwfn_3.8_dev_bin_Linux_noGUI/data.txt | tee ./"
+                    + QTAIM_loc_reactant
+                    + "out \n"
+                )
 
             # run QTAIM on products
-            with open(QTAIM_loc_product + '/props.sh', 'w') as f: 
+            with open(QTAIM_loc_product + "/props.sh", "w") as f:
                 f.write("#!/bin/bash\n")
-                f.write("./Multiwfn_3.8_dev_bin_Linux_noGUI/Multiwfn " + QTAIM_loc_product + "/input.wfn < ./Multiwfn_3.8_dev_bin_Linux_noGUI/data.txt | tee ./" + QTAIM_loc_product + "out \n")
+                f.write(
+                    "./Multiwfn_3.8_dev_bin_Linux_noGUI/Multiwfn "
+                    + QTAIM_loc_product
+                    + "/input.wfn < ./Multiwfn_3.8_dev_bin_Linux_noGUI/data.txt | tee ./"
+                    + QTAIM_loc_product
+                    + "out \n"
+                )
 
-    else:   
+            st = os.stat(QTAIM_loc_product + "/props.sh")
+            os.chmod(QTAIM_loc_product + "/props.sh", st.st_mode | stat.S_IEXEC)
+
+            st = os.stat(QTAIM_loc_reactant + "/props.sh")
+            os.chmod(QTAIM_loc_reactant + "/props.sh", st.st_mode | stat.S_IEXEC)
+    else:
         dir_source = "./"
-        files = glob(dir_source+"*xyz") # xyz file names, would need to change for pandas
+        files = glob(
+            dir_source + "*xyz"
+        )  # xyz file names, would need to change for pandas
         for i in files:
-            folder = i.split('_')[1].split('.')[0]
-            try: 
+            folder = i.split("_")[1].split(".")[0]
+            try:
                 os.mkdir(folder)
-            except: 
+            except:
                 pass
 
             with open(i) as xyz:
@@ -91,19 +127,35 @@ def main():
 
             atoms = int(lines[0])
 
-            with open(folder + "/input.in", 'w') as f:
+            with open(folder + "/input.in", "w") as f:
                 f.write("!B3LYP def2-SVP AIM\n\n")
                 f.write("*xyz 0 1\n")
-                for ind in range(atoms): 
-                    f.write(str(lines[ind+2].split()[0]) + "\t" + str(lines[ind+2].split()[1]) + "\t" + str(lines[ind+2].split()[1]) + "\t" + str(lines[ind+2].split()[2]) + "\n")        
+                for ind in range(atoms):
+                    f.write(
+                        str(lines[ind + 2].split()[0])
+                        + "\t"
+                        + str(lines[ind + 2].split()[1])
+                        + "\t"
+                        + str(lines[ind + 2].split()[1])
+                        + "\t"
+                        + str(lines[ind + 2].split()[2])
+                        + "\n"
+                    )
                 f.write("*\n")
 
-            with open(folder + '/props.sh', 'w') as f: 
-                #print("#!/bin/bash\n")
-                #print("../Multiwfn_3.8_dev_bin_Linux_noGUI/Multiwfn input.wfn < ../Multiwfn_3.8_dev_bin_Linux_noGUI/data.txt\n")
+            with open(folder + "/props.sh", "w") as f:
+                # print("#!/bin/bash\n")
+                # print("../Multiwfn_3.8_dev_bin_Linux_noGUI/Multiwfn input.wfn < ../Multiwfn_3.8_dev_bin_Linux_noGUI/data.txt\n")
                 f.write("#!/bin/bash\n")
-                f.write("./Multiwfn_3.8_dev_bin_Linux_noGUI/Multiwfn " + folder + "/input.wfn < ./Multiwfn_3.8_dev_bin_Linux_noGUI/data.txt | tee ./" +folder+"/out \n")
+                f.write(
+                    "./Multiwfn_3.8_dev_bin_Linux_noGUI/Multiwfn "
+                    + folder
+                    + "/input.wfn < ./Multiwfn_3.8_dev_bin_Linux_noGUI/data.txt | tee ./"
+                    + folder
+                    + "/out \n"
+                )
 
-            #print(i.split("_")[1].split('.')[0])
+            # print(i.split("_")[1].split('.')[0])
+
 
 main()
