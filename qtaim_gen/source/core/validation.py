98ca55b1c886775001f4d21c3b5feb486f9067a4
Santiago Vargas <santiagovargas921@gmail.com>
2025-07-28 13:14:49 -0400

    verbosity + inp check

--- qtaim_gen/source/core/validation.py
+++ qtaim_gen/source/core/validation.py
@@ -2,7 +2,7 @@ import os
 import json 
 from qtaim_gen.source.core.parse_qtaim import dft_inp_to_dict
 
-def validate_timing_dict(timing_json_loc): 
+def validate_timing_dict(timing_json_loc, verbose=False): 
     """
     Basic check that the timing json file has the expected structure.
     Check that it has the keys 'total', 'qtaim', 'charge', 'bond', and 'fuzzy_full'.
@@ -24,27 +24,40 @@ def validate_timing_dict(timing_json_loc):
         'fuzzy_bond',
         'ibsi_bond',
         'becke_fuzzy_density',
-        'becke_fuzzy_spin',
         'mbis_fuzzy_density',
-        'mbis_fuzzy_spin',
         'hirsh_fuzzy_density',
-        'hirsh_fuzzy_spin',
         'grad_norm_rho_fuzzy',
         'laplacian_rho_fuzzy',
         'elf_fuzzy']
+
+    excepted_spin_keys = [
+        'mbis_fuzzy_spin',
+        'hirsh_fuzzy_spin',
+        'becke_fuzzy_spin',
+    ]
     for key in expected_keys:
         if key not in timing_dict:
-            raise ValueError(f"Missing expected key '{key}' in timing json.")
-        # check that the times aren't tiny 
+            if verbose: 
+                print(f"Missing expected key '{key}' in timing json.")
+            return False
         
+        # check that the times aren't tiny 
         if timing_dict[key] < 1e-6 and key != 'convert':
             print(f"Timing for '{key}' is too small: {timing_dict[key]} seconds.")
+            return 
+    
+    for key in excepted_spin_keys:
+        if key not in timing_dict:
+            if verbose:
+                print(f"Missing expected spin key '{key}' in timing json.")
             return False
-    print("Timing json structure is valid.")
+    
+    if verbose:
+        print("Timing json structure is valid.")
     return True
     
 
-def validate_fuzzy_dict(fuzzy_json_loc, n_atoms=None, spin=True):
+def validate_fuzzy_dict(fuzzy_json_loc, n_atoms=None, spin=True, verbose=False):
     """
     Basic check that the fuzzy json file has the expected structure.
     Check that it has the keys 'fuzzy', 'fuzzy_bonds', 'fuzzy_bcp', 'fuzzy_ncp'.
@@ -60,12 +73,15 @@ def validate_fuzzy_dict(fuzzy_json_loc, n_atoms=None, spin=True):
             raise ValueError(f"Missing expected key '{key}' in fuzzy json.")
         if n_atoms is not None:
             if len(fuzzy_dict[key]) != n_atoms + 2:
-                print(f"Number of fuzzy points ({len(fuzzy_dict[key])}) does not match expected ({n_atoms}).")
+                if verbose:
+                    print(f"Number of fuzzy points ({len(fuzzy_dict[key])}) does not match expected ({n_atoms}).")
                 return False
-    print("Fuzzy json structure is valid.")
+    if verbose: 
+        print("Fuzzy json structure is valid.")
     return True
 
-def validate_other_dict(other_dict_loc):
+
+def validate_other_dict(other_dict_loc, verbose=False):
     """
     Basic check that the other json file has the expected structure.
     Check that it has the keys 'atoms', 'bonds', 'charges', and 'fuzzy'.
@@ -75,15 +91,16 @@ def validate_other_dict(other_dict_loc):
     expected_keys = ['mpp_full', 'sdp_full', 'mpp_heavy', 'sdp_heavy', 'ESP_Volume', 'ESP_Surface_Density', 'ESP_Minimal_value', 'ESP_Maximal_value', 'ESP_Overall_surface_area', 'ESP_Positive_surface_area', 'ESP_Negative_surface_area', 'ESP_Overall_average_value', 'ESP_Positive_average_value', 'ESP_Negative_average_value', 'ESP_Overall_variance', 'ESP_Positive_variance', 'ESP_Negative_variance', 'ESP_Balance_of_charges', 'ESP_Product_of_sigma', 'ESP_Internal_charge_separation', 'ESP_Molecular_polarity_index', 'ESP_Nonpolar_surface_area', 'ESP_Polar_surface_area', 'ESP_Overall_skewness', 'ESP_Positive_skewness', 'ALIE_Volume', 'ALIE_Surface_Density', 'ALIE_Minimal_value', 'ALIE_Maximal_value', 'ALIE_Overall_surface_area', 'ALIE_Positive_surface_area', 'ALIE_Negative_surface_area', 'ALIE_Overall_skewness', 'ALIE_Positive_skewness']
     for key in expected_keys:
         if key not in other_dict:
-            print(f"Warning: Missing expected key '{key}' in other json. This may not be critical.")
+            if verbose:
+                print(f"Warning: Missing expected key '{key}' in other json. This may not be critical.")
             return False
         
-            
-    print("Other json structure is valid.")
+    if verbose:
+        print("Other json structure is valid.")   
     return True
 
 
-def validate_charge_dict(charge_json_loc, n_atoms=None):
+def validate_charge_dict(charge_json_loc, n_atoms=None, verbose=False):
     """
     Basic check that the charge json file has the expected structure.
     Check that it has the keys 'mbis', 'adch', 'chelpg', 'becke',  'hirshfeld', 'cm5', 'bader', 'vdd'
@@ -95,22 +112,26 @@ def validate_charge_dict(charge_json_loc, n_atoms=None):
     expected_keys = ['mbis', 'adch', 'chelpg', 'becke', 'hirshfeld', 'cm5', 'bader', 'vdd']
     for key in expected_keys:
         if key not in charge_dict:
-            print(f"Missing expected key '{key}' in charge json.")
+            if verbose:
+                print(f"Warning: Missing expected key '{key}' in charge json. ")
             return False
 
     for key in expected_keys:
         if 'charge' not in charge_dict[key]:
-            print(f"Missing 'charge' key in '{key}' of charge json.")
+            if verbose:
+                print(f"Missing 'charge' key in '{key}' of charge json.")
             return False
         if n_atoms is not None:
             if len(charge_dict[key]['charge']) != n_atoms:
-                print(f"Number of charges in '{key}' ({len(charge_dict[key]['charge'])}) does not match expected ({n_atoms}).")
+                if verbose:
+                    print(f"Number of charges in '{key}' ({len(charge_dict[key]['charge'])}) does not match expected ({n_atoms}).")
                 return False
-    print("Charge json structure is valid.")
+    if verbose:
+        print("Charge json structure is valid.")
     return True
 
 
-def validate_qtaim_dict(qtaim_json_loc, n_atoms=None, harsh_check=False):
+def validate_qtaim_dict(qtaim_json_loc, n_atoms=None, verbose=False):
     """
     Basic check that the qtaim json file has the expected structure
     Check that it has the keys 'atoms', 'bonds', 'charges', and 'fuzzy'.
@@ -121,7 +142,8 @@ def validate_qtaim_dict(qtaim_json_loc, n_atoms=None, harsh_check=False):
         qtaim_dict = json.load(f)
     # check it isn't empty
     if not qtaim_dict:
-        print("QTAIM json file is empty.")
+        if verbose:
+            print("QTAIM json file is empty.")
         return False
     
     #dict_ncps = {qtaim_dict[key] for key in qtaim_dict if "_" not in key}
@@ -131,36 +153,52 @@ def validate_qtaim_dict(qtaim_json_loc, n_atoms=None, harsh_check=False):
 
     if n_atoms is not None:
         if len(dict_ncps) != n_atoms:
-            print(f"Number of non-bonded critical points ({len(dict_ncps)}) does not match expected ({n_atoms}).")
-            if harsh_check:
-                return False
-    print("QTAIM json structure is valid.")
+            if verbose:
+                print(f"Number of nuclear critical points ({len(dict_ncps)}) does not match expected ({n_atoms}).")
+            return False
+    if verbose:
+        print(f"Number of nuclear critical points: {len(dict_ncps)}")
+        print(f"Number of bond critical points: {len(dict_bcps)}")
+        print("QTAIM json structure is valid.")
     return True
     
 
-
-def validation_checks(folder): 
+def validation_checks(folder, verbose=False): 
     """
     Run all validation checks on the json files in the given folder.
     """
     # check that all the json files are present
-    required_files = ["timings.json", "fuzzy_full.json", "other.json", "charge.json", "qtaim.json", "orca.inp"]
+    required_files = ["timings.json", "fuzzy_full.json", "other.json", "charge.json", "qtaim.json"]
     for file in required_files:
         if not os.path.exists(os.path.join(folder, file)):
             print(f"Missing required file: {file}")
             return False
 
+    # check for a file ending with .inp
+    inp_files = [f for f in os.listdir(folder) if f.endswith('.inp')]
+    if not inp_files:
+        if verbose:
+            print("No .inp file found in the folder.")
+        return False
+    inp_file = inp_files[0]  # take the first .inp file found
+    if verbose:
+        print(f"Using input file \"{inp_file}\" for validation.")
+
     # gather n_atoms, spin, charge from the orca.inp file
-    orca_inp_path = os.path.join(folder, "orca.inp")
+    orca_inp_path = os.path.join(folder, inp_file) # might need to change this name 
     if not os.path.exists(orca_inp_path):
-        print("Missing orca.inp file.")
+        if verbose:
+            print(f"Missing orca.inp file at {orca_inp_path}.")
         return False
     dft_dict = dft_inp_to_dict(orca_inp_path, parse_charge_spin=True)
     #print(dft_dict)
     n_atoms = len(dft_dict["mol"])
     spin = dft_dict.get("spin", None)
     charge = dft_dict.get("charge", None)
-    print(f"n_atoms: {n_atoms}, spin: {spin}, charge: {charge}")
+    
+    if verbose:
+        print(f"n_atoms: {n_atoms}, spin: {spin}, charge: {charge}")
+    
     if spin != 2:
         spin_tf = True
     else:
@@ -173,19 +211,18 @@ def validation_checks(folder):
     qtaim_json_loc = os.path.join(folder, "qtaim.json")
     #bonding_json_loc = os.path.join(folder, "bonding.json")
 
-    if not validate_timing_dict(timing_json_loc):
+    if not validate_timing_dict(timing_json_loc, verbose=verbose):
         return False
-    if not validate_fuzzy_dict(fuzzy_json_loc, n_atoms=n_atoms, spin=spin_tf):
+    if not validate_fuzzy_dict(fuzzy_json_loc, n_atoms=n_atoms, spin=spin_tf, verbose=verbose):
         return False
-    if not validate_other_dict(other_dict_loc):
+    if not validate_other_dict(other_dict_loc, verbose=verbose):
         return False
-    if not validate_charge_dict(charge_json_loc, n_atoms=n_atoms):
+    if not validate_charge_dict(charge_json_loc, n_atoms=n_atoms, verbose=verbose):
         return False
-    if not validate_qtaim_dict(qtaim_json_loc, n_atoms=n_atoms, harsh_check=True):
+    if not validate_qtaim_dict(qtaim_json_loc, n_atoms=n_atoms, verbose=verbose):
         return False
-    #if not validate_qtaim_dict(bonding_json_loc, n_atoms=n_atoms, harsh_check=False):
-    #    return False
-    
-    print("All validation checks passed.")
+
+    if verbose:
+        print("All validation checks passed.")
     return True
     
