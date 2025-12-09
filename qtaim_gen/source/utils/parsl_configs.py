b09bb58c0bd77e8dc1ab61493a6af92f3816837a
Santiago Vargas <santiagovargas921@gmail.com>
2025-12-09 11:37:15 -0500

    ppn opt

--- qtaim_gen/source/utils/parsl_configs.py
+++ qtaim_gen/source/utils/parsl_configs.py
@@ -91,7 +91,7 @@ def alcf_config(
                     # Change if data/modules located on other filesystem
                     scheduler_options="#PBS -l filesystems=home:eagle",
                     # Ensures 1 manger per node; the manager will distribute work to its 12 workers, one per tile
-                    launcher=MpiExecLauncher(bind_cmd="--cpu-bind"),
+                    launcher=MpiExecLauncher(bind_cmd="--cpu-bind", overrides="--ppn 1"),
                     # options added to #PBS -l select aside from ncpus
                     select_options="",
                     # How many nodes per PBS job:
@@ -112,6 +112,110 @@ def alcf_config(
     )
     return aurora_single_tile_config, threads_per_node
 
+
+def alcf_config_single_pbs(
+    threads_per_task: int = 8,
+    safety_factor: float = 1.0,
+    threads_per_node: int = 256,
+    n_jobs: int = 64,
+    queue: str = "debug",
+    walltime: str = "00:30:00",
+    monitoring: bool = False,
+) -> Config:
+    """
+    Returns a Parsl config optimized for running on ALCF.
+
+    Returns:
+        Config: A Parsl configuration object for ALCF.
+    """
+    # These options will run work in 1 node batch jobs run one at a time
+
+    # threads_per_node   = 256        # hardware threads
+    # threads_per_task   = 8          # each job uses 8 threads
+    workers_per_node = int(
+        threads_per_node // threads_per_task // safety_factor
+    )  # 256 // 8 = 32
+
+    nodes_per_job = 1
+
+    # The config will launch workers from this directory
+    execute_dir = os.getcwd()
+
+    if monitoring:
+        monitoring = MonitoringHub(
+            hub_address=address_by_hostname(),
+            hub_port=55055,
+            monitoring_debug=False,
+        )
+    else:
+        monitoring = None
+
+    aurora_single_tile_config = Config(
+        executors=[
+            HighThroughputExecutor(
+                label="htex_cpu",
+                # Ensures one worker per GPU tile on each node
+                max_workers_per_node=workers_per_node,
+                cpu_affinity="block",
+                prefetch_capacity=0,
+                # Options that specify properties of PBS Jobs
+                provider=PBSProProvider(
+                    # Project name
+                    account="generator",
+                    # Submission queue
+                    queue=queue,
+                    # Commands run before workers launched
+                    # Make sure to activate your environment where Parsl is installed
+                    worker_init=(                      # Debugging
+                        "set -x; "  # print every command as it runs
+                        "echo '--- WORKER_INIT START ---'; "
+                        "hostname; "
+                        "date; "
+                        "module use /soft/modulefiles; "
+                        "source /soft/datascience/conda/2025-06-03/etc/profile.d/conda.sh; "
+                        "echo 'Loaded conda module'; "
+                        # Sanity check Python / Parsl
+                        "which python || { echo 'python not found'; exit 1; }; "
+                        "python -c 'import sys, parsl; "
+                        "print(\"PYOK\", sys.version); "
+                        "print(\"PARSL\", parsl.__version__)' || { echo 'Python/parsl import failed'; exit 1; }; "
+                        # Go to working dir
+                        f"cd {execute_dir} || {{ echo 'cd {execute_dir} failed'; exit 1; }}; "
+                        "pwd; "
+                        f"export OMP_NUM_THREADS={threads_per_task}; "
+                        f"export OMP_STACKSIZE={threads_per_task*1024}M; "
+                        f"export OPENBLAS_NUM_THREADS={threads_per_task}; "
+                        f"export OMP_PROC_BIND=true; "
+                        f"export OMP_PLACES=cores ; "
+                        f"export MKL_NUM_THREADS={threads_per_task}; "
+                        "ulimit -s 300000; " 
+                        "export KMP_STACKSIZE=200M; "
+                    ),
+                    # Wall time for batch jobs
+                    walltime=walltime, 
+                    # Change if data/modules located on other filesystem
+                    scheduler_options="#PBS -l filesystems=home:eagle",
+                    # Ensures 1 manger per node; the manager will distribute work to its 12 workers, one per tile
+                    launcher=MpiExecLauncher(bind_cmd="--cpu-bind", overrides="--ppn 1"),
+                    # options added to #PBS -l select aside from ncpus
+                    select_options="",
+                    # How many nodes per PBS job:
+                    nodes_per_block=nodes_per_job,
+                    # Min/max *concurrent* PBS jobs (blocks) that Parsl can have in the queue:
+                    min_blocks=1,
+                    max_blocks=n_jobs,
+                    # Tell Parsl / PBS how many hardware threads there are per node:
+                    cpus_per_node=threads_per_node,
+                ),
+            ),
+        ],
+        # How many times to retry failed tasks
+        # this is necessary if you have tasks that are interrupted by a PBS job ending
+        # so that they will restart in the next job
+        retries=1,
+        #monitoring=monitoring,
+    )
+    return aurora_single_tile_config, threads_per_node
 
 def base_config(n_workers: int = 4) -> Config:
     """Returns a basic Parsl config using local threads executor.
